4950fe55ba8ffdd1948324caa246b2db2fadbfde
Mike <mblack101@ufl.edu>
2019-10-26 18:48:32 -0400

    Show and hide classes based on user input

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const TemperatureStandards = {
   BOILING_POINT: '212',
-  FREEZING_POINT:'32'
+  FREEZING_POINT: '32'
 }
 
 const getBoilingTemp = (temp) => {
@@ -11,23 +11,50 @@ const getBoilingTemp = (temp) => {
   spanBoilingTemp.appendChild(textNode);
 }
 
-
 const getFreezingTemp = (temp) => {
   const spanFreezingTemp = document.getElementById('freezing-temp');
   const textNode = document.createTextNode(temp);  
   spanFreezingTemp.appendChild(textNode);
 }
 
+const evaluateTemp = (inputTemp) => {
+  let status = null;
+  if (inputTemp > 212) {
+    status = 'boiling';
+  } else if (inputTemp < 32) {
+    status = 'frozen';
+  } else {
+    status = 'good-temp';
+  }
+
+  showStatus(status);
+}
+
+const showStatus = (status) => {
+  resetStatus();
+  const sectionStatus = document.getElementById(status);
+  sectionStatus.classList.remove('hide');
+  sectionStatus.classList.add('show');
+}
+
+function resetStatus () {
+  sectionStatusList = document.getElementsByTagName('footer')[0].childNodes;
+
+  for (var i = 0, len = sectionStatusList.Length; i < len; ++i) {
+    if (sectionStatusList[i].classList.contains('show')) {
+      sectionStatusList[i].classList.remove('show');
+    }
+  }
+}
 
 function setTemp() { 
-  console.log('setTemp()');
   const inputTemp = document.getElementById('input-temp').value;
   const spanWaterTemp = document.getElementById('water-temp');
   const textNode = document.createTextNode(inputTemp);
   spanWaterTemp.appendChild(textNode);
 
   setTimeout(() => {
-    alert('yo');
+    evaluateTemp(inputTemp);
   }, 3000);
 }
 
