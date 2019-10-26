024ba443ae21fadaebecedae1546af0a32df590d
Mike <mblack101@ufl.edu>
2019-10-26 18:53:45 -0400

    Add comments to javascript

--- main.js
+++ main.js
@@ -5,18 +5,21 @@ const TemperatureStandards = {
   FREEZING_POINT: '32'
 }
 
+// Update page with constant boiling temp
 const getBoilingTemp = (temp) => {
   const spanBoilingTemp = document.getElementById('boiling-temp');
   const textNode = document.createTextNode(temp);  
   spanBoilingTemp.appendChild(textNode);
 }
 
+// Update page with constant freezing temp
 const getFreezingTemp = (temp) => {
   const spanFreezingTemp = document.getElementById('freezing-temp');
   const textNode = document.createTextNode(temp);  
   spanFreezingTemp.appendChild(textNode);
 }
 
+// Determine if user temp is boiling, frozen, or good
 const evaluateTemp = (inputTemp) => {
   let status = null;
   if (inputTemp > 212) {
@@ -26,10 +29,10 @@ const evaluateTemp = (inputTemp) => {
   } else {
     status = 'good-temp';
   }
-
   showStatus(status);
 }
 
+// Display status by adding the .show class for the corresponding section
 const showStatus = (status) => {
   resetStatus();
   const sectionStatus = document.getElementById(status);
@@ -37,16 +40,18 @@ const showStatus = (status) => {
   sectionStatus.classList.add('show');
 }
 
+// Reset all sections to hidden, so only the correct one appears at a time 
 function resetStatus () {
   sectionStatusList = document.getElementsByTagName('footer')[0].childNodes;
-
   for (var i = 0, len = sectionStatusList.Length; i < len; ++i) {
     if (sectionStatusList[i].classList.contains('show')) {
       sectionStatusList[i].classList.remove('show');
+      sectionStatusList[i].classList.add('hide');
     }
   }
 }
 
+// Set temp based on user input and then pass it to be evaluated
 function setTemp() { 
   const inputTemp = document.getElementById('input-temp').value;
   const spanWaterTemp = document.getElementById('water-temp');
