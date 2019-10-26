353720c4c9e65b7beef40fdc3f3b27568c8dd033
Mike <mblack101@ufl.edu>
2019-10-26 16:53:51 -0400

    Add getBoiling and getFreezing functions

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 // It is always helpful to use comments in your code!
+
+const Temperatures = {
+  BOILING_POINT: '212',
+  FREEZING_POINT:'32'
+}
+
+const getBoilingTemp = (temp) => {
+  const spanBoilingTemp = document.getElementById('boiling-temp');
+  const textNode = document.createTextNode(temp);  
+  spanBoilingTemp.appendChild(textNode);
+}
+
+
+const getFreezingTemp = (temp) => {
+  const spanFreezingTemp = document.getElementById('freezing-temp');
+  const textNode = document.createTextNode(temp);  
+  spanFreezingTemp.appendChild(textNode);
+}
+
+
+getBoilingTemp(Temperatures.BOILING_POINT);
+getFreezingTemp(Temperatures.FREEZING_POINT);
+
